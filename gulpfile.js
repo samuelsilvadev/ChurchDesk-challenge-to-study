4247ffe44da27eb3e1aaa651f1247efbafc384da
Samuel SIlva <samuelsilvawb@gmail.com>
2018-03-24 16:34:38 -0300

    chore : add browserify dependencie
    - added task to build with browserify
    - added task to use babel in deploy

--- gulpfile.js
+++ gulpfile.js
@@ -9,46 +9,67 @@ const imagemin = require('gulp-imagemin');
 const minifyCSS = require('gulp-clean-css');
 const browserSync = require('browser-sync');
 const autoprefixer = require('gulp-autoprefixer');
-const gulpSequence = require('gulp-sequence').use(gulp);
 const shell = require('gulp-shell');
 const plumber = require('gulp-plumber');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const babel = require('gulp-babel');
+const runSequence = require('run-sequence');
 
 gulp.task('browserSync', () => {
 	browserSync({
 		server: {
 			baseDir: "./app/"
-		},		
+		},
 		notify: false
 	});
 });
 
 gulp.task('images', (tmp) => {
-	gulp.src(['app/images/*.jpg', 'app/images/*.png'])
+	return gulp.src(['app/images/*.jpg', 'app/images/*.png'])
 		.pipe(plumber())
 		.pipe(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true }))
 		.pipe(gulp.dest('app/images'));
 });
 
 gulp.task('images-deploy', () => {
-	gulp.src(['app/images/**/*', '!app/images/README'])
+	return gulp.src(['app/images/**/*', '!app/images/README'])
 		.pipe(plumber())
 		.pipe(gulp.dest('app/dist/images'));
 });
 
 gulp.task('scripts', () => {
-	return gulp.src(['app/scripts/**/*.js'])
-		.pipe(plumber())		
+	return browserify(['app/scripts/app.js'])
+		.bundle()
+		.pipe(source('app.js'))
+		.pipe(plumber())
 		.on('error', console.error)
 		.pipe(gulp.dest('app/dist/scripts'))
 		.pipe(browserSync.reload({ stream: true }));
 });
 
 gulp.task('scripts-deploy', () => {
-	return gulp.src(['app/scripts/**/*.js'])
+	return browserify(['app/scripts/app.js'])
+		.bundle()
+		.pipe(source('app.js'))
 		.pipe(plumber())
-		.pipe(concat('app.js'))
-		.pipe(uglify())
 		.pipe(gulp.dest('app/dist/scripts'));
+});
+
+gulp.task('scripts-deploy-babel', () => {
+	return gulp.src('app/dist/scripts/app.js')
+		.pipe(babel({
+			presets: ['env']
+		}))
+		.pipe(gulp.dest(file => file.base));
+	}
+);
+
+gulp.task('scripts-deploy-end', () => {
+	return gulp.src(['app/dist/scripts/app.js'])
+		.pipe(plumber())		
+		.pipe(uglify())
+		.pipe(gulp.dest(file => file.base));
 });
 
 gulp.task('styles', () => {
@@ -120,7 +141,7 @@ gulp.task('html-deploy', () => {
 
 gulp.task('clean', () => {
 	return shell.task([
-		'rm -rf app/dist'
+		'rm -rf ./app/dist'
 	]);
 });
 
@@ -131,16 +152,16 @@ gulp.task('scaffold', () => {
 		'mkdir app/dist/images',
 		'mkdir app/dist/scripts',
 		'mkdir app/dist/styles'
-	]
-	);
+	]);
 });
 
-gulp.task('default', ['browserSync', 'scripts', 'styles'], () => {
-	//a list of watchers, so it will watch all of the following files waiting for changes
+gulp.task('default', ['browserSync', 'scripts', 'styles'], () => {	
 	gulp.watch('app/scripts/**', ['scripts']);
 	gulp.watch('app/styles/**', ['styles']);
 	gulp.watch('app/images/**', ['images']);
 	gulp.watch('app/*.html', ['html']);
 });
 
-gulp.task('deploy', gulpSequence('clean', 'scaffold', ['scripts-deploy', 'styles-deploy', 'images-deploy'], 'html-deploy'));
+gulp.task('deploy', () => {
+	runSequence('clean', 'scaffold', 'scripts-deploy', 'scripts-deploy-babel', 'scripts-deploy-end', 'styles-deploy', 'images-deploy', 'html-deploy')
+});
